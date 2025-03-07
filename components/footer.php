45cbb19fce5d2b9b57a3f5f361c6168a2a3fa4fc
MnzaJvd <mnzajaved@gmail.com>
2025-03-07 22:50:34 +0500

    Update footer.php

--- components/footer.php
+++ components/footer.php
@@ -1,6 +1,7 @@
 <footer class="position-relative bg-dark text-white text-center py-3 mt-auto">
     <p class="mb-0">© <?php echo date('Y'); ?> Online Parking. All rights reserved.</p>
 </footer>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
