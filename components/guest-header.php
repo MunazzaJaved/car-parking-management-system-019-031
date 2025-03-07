008a6d28e0c6c68891a1681dd807be968d11707f
MnzaJvd <mnzajaved@gmail.com>
2025-03-07 23:08:19 +0500

    Update guest-header.php

--- components/guest-header.php
+++ components/guest-header.php
@@ -6,19 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Online Parking - Guest</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
-
+    <link rel="stylesheet" href="../assets/style.css">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
     <header class="bg-dark shadow-sm ">
         <nav class="navbar navbar-expand-lg navbar-dark container">
             <a class="navbar-brand text-white">&nbsp; Online Parking</a>
-            <button class="navbar-toggler d-flex ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarNav" style="margin-right: 20px;">
-                <div class="d-flex justify-content-end">
+                <div class="nav-responsive">
                     <ul class="navbar-nav ms-auto">
                         <li class="nav-item">
                             <a class="nav-link text-white" href="../../view/guest/home.php">Home</a>
