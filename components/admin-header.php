93d7d93b47dac7a1939bea13d3b94bfb76c65636
MnzaJvd <mnzajaved@gmail.com>
2025-03-08 12:56:17 +0500

    Update admin-header.php

--- components/admin-header.php
+++ components/admin-header.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>parking - admin</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/car-parking-management/assets/nav-style.css">
 </head>
 
 <body class="d-flex flex-column min-vh-100">
